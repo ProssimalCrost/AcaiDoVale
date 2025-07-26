b5551ccaffa53e8a29a990988d38aeab806343c9
Theylon Augusto <theylon.silva@hotmail.com>
2025-07-25 23:59:22 -0300

    Fixed version tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  content: [],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    require('flowbite/plugin'),
+  ],
+};
